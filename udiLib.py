443bdcb18a8dd969d78c0c7fe5ae9d97247b3cf4
Panda88CO <christian.olgaard@gmail.com>
2024-05-06 13:50:12 -0700

    Update udiLib.py

--- udiLib.py
+++ udiLib.py
@@ -209,11 +209,11 @@ def tempUnitAdjust(self, tempC):
 def setDriverTemp(self, Id, value):
     logging.debug('setDriverTemp : TempUnit: {}, value: {}'.format(self.TEV.teslaEV_GetTempUnit(), value))
     if value == None:
-        self.ev_setDriver(Id, 99,25)  
+        self.EV_setDriver(Id, 99,25)  
     elif self.TEV.teslaEV_GetTempUnit()  == 0:
-        self.ev_setDriver(Id, round(round(2*value,0)/2,1),4)
+        self.EV_setDriver(Id, round(round(2*value,0)/2,1),4)
     elif self.TEV.teslaEV_GetTempUnit()  == 1:
-        self.ev_setDriver(Id, round(32+ 1.8*value, 0),17)
+        self.EV_setDriver(Id, round(32+ 1.8*value, 0),17)
     else:
         return (None)
 
